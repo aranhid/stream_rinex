ea489239e5882e60b48bf282fca87d1f2205f3f6
Владислав Цыбуля <aranhid@yandex.ru>
2022-05-18 21:23:49 +0800

    sendStream: add KeyboardInterrupt try-except

--- sendStream.py
+++ sendStream.py
@@ -40,49 +40,51 @@ def main():
     speed = args.speed
     from_current_time = args.from_current_time
 
-    conf = {'bootstrap.servers': f"{host}:9092",
-            'client.id': socket.gethostname()}
-    producer = Producer(conf)
+    try:
+        conf = {'bootstrap.servers': f"{host}:9092",
+                'client.id': socket.gethostname()}
+        producer = Producer(conf)
 
-    interval = timedelta(seconds=args.interval)
-    working_df = get_dataframe(args.files, interval)
+        interval = timedelta(seconds=args.interval)
+        working_df = get_dataframe(args.files, interval)
 
-    timestamps = np.unique(working_df['Timestamp'].dt.to_pydatetime())
+        timestamps = np.unique(working_df['Timestamp'].dt.to_pydatetime())
 
-    if from_current_time:
-        current_gps_time = datetime.utcnow()
-        timestamps = [ts for ts in timestamps if ts.time() >= current_gps_time.time()]
-        sec_to_sleep = timedelta(hours=timestamps[0].time().hour - current_gps_time.time().hour,
-                                 minutes=timestamps[0].time().minute - current_gps_time.time().minute,
-                                 seconds=timestamps[0].time().second - current_gps_time.time().second).seconds
-        time.sleep(sec_to_sleep)
+        if from_current_time:
+            current_gps_time = datetime.utcnow()
+            timestamps = [ts for ts in timestamps if ts.time() >= current_gps_time.time()]
+            sec_to_sleep = timedelta(hours=timestamps[0].time().hour - current_gps_time.time().hour,
+                                    minutes=timestamps[0].time().minute - current_gps_time.time().minute,
+                                    seconds=timestamps[0].time().second - current_gps_time.time().second).seconds
+            time.sleep(sec_to_sleep)
 
-    for timestamp in timestamps:
-        time_df = working_df[working_df['Timestamp'] == timestamp]
-        for index, row in time_df.iterrows():
-            if row['Satellite'][0] == 'G':
-                if not math.isnan(row['P range tec']) and not math.isnan(row['Phase tec']):
-                    payload = create_payload(row)
+        for timestamp in timestamps:
+            time_df = working_df[working_df['Timestamp'] == timestamp]
+            for index, row in time_df.iterrows():
+                if row['Satellite'][0] == 'G':
+                    if not math.isnan(row['P range tec']) and not math.isnan(row['Phase tec']):
+                        payload = create_payload(row)
 
-                    debug_json = {}
-                    debug_json['Timestamp'] = str(row['Timestamp'].to_pydatetime())
-                    debug_json['Satellite'] = row['Satellite']
-                    debug_json['P range tec'] = row['P range tec']
-                    debug_json['P range 1'] = row['P range'].get(1)
-                    debug_json['P range 2'] = row['P range'].get(2)
-                    debug_json['Phase tec'] = row['Phase tec']
-                    debug_json['Phase 1'] = row['Phase'].get(1)
-                    debug_json['Phase 2'] = row['Phase'].get(2)
-                    debug_json['bin_message'] =  base64.b64encode(payload).decode("utf8")
-                
-                    jresult = json.dumps(debug_json)
-
-                    if payload is not None:
-                        producer.produce(topic, key=p_key, value=jresult, callback=acked)
+                        debug_json = {}
+                        debug_json['Timestamp'] = str(row['Timestamp'].to_pydatetime())
+                        debug_json['Satellite'] = row['Satellite']
+                        debug_json['P range tec'] = row['P range tec']
+                        debug_json['P range 1'] = row['P range'].get(1)
+                        debug_json['P range 2'] = row['P range'].get(2)
+                        debug_json['Phase tec'] = row['Phase tec']
+                        debug_json['Phase 1'] = row['Phase'].get(1)
+                        debug_json['Phase 2'] = row['Phase'].get(2)
+                        debug_json['bin_message'] =  base64.b64encode(payload).decode("utf8")
                     
-        producer.flush()
-        time.sleep(interval.total_seconds() / speed)
+                        jresult = json.dumps(debug_json)
 
+                        if payload is not None:
+                            producer.produce(topic, key=p_key, value=jresult, callback=acked)
+                        
+            producer.flush()
+            time.sleep(interval.total_seconds() / speed)
+    except KeyboardInterrupt:
+        pass
 
 if __name__ == "__main__":
     main()
